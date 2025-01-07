f4f47f3772a9dbb0f76260e4fdae72ea924089b6
moisesmarq <moises.marques@outlook.com>
2025-01-07 18:57:39 -0300

    implement data processing

--- juros.js
+++ juros.js
@@ -3,8 +3,16 @@ import rlsync from 'readline-sync';
 
 console.log("Aplicação de Juros:\n");
 
+
 let valor_devido = rlsync.questionFloat("Informe o valor devido: R$ ");
 let qtd_dias_passados = rlsync.questionInt("Informe quantos dias se passaram desde o vencimento do boleto: ");
 
-console.log("Valor original da dívida: R$ " + valor_devido);
+let juros = 10;
+
+let valor_juros = (valor_devido / 100) * juros ;
+let valor_total = valor_devido + valor_juros ;
+
+console.log("\nValor original da dívida: R$ " + valor_devido);
 console.log("Dias atrasados: " + qtd_dias_passados);
+console.log("Taxa de Juros: " + valor_juros + "%");
+console.log("Valor total com juros: R$ " + valor_total)
